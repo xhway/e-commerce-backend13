a59c3dc8b9e49ca72a2a766e33a502560decadf9
Xhway <dhway20@gmail.com>
2023-04-14 09:52:04 -0400

    final code for categoryroutes file

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,70 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+
+router.get('/', async (req, res) => {
+  await Category.findAll({
+    attributes: ["id", "category_name"],
+    include:[{
+      model: Product,
+      attributes: ["id", "product_name", "price","stock", "category_id"]
+    }]
+  })
+  .then((categories)=>{
+    res.json(categories);
+  })
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+ await Category.findByPk(req.params.id, {
+  attributes: ["id", "category_name"],
+  include:[{
+    model: Product,
+    attributes: ["id","product_name","price","stock","category_id"],
+  }],
+ 
+ })
+ .then((category)=>{
+  res.json(category);
+})
+.catch((err)=>{
+  res.json(err);
+});
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+  await Category.create(req.body)
+  .then((newCategory) => res.status(200).json(newCategory))
+  .catch((err) => {
+    console.log(err);
+    res.status(400).json(err);
+  });
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  await Category.update(req.body, {
+    where: {
+      id:req.params.id,
+    },
+  })
+  .then(cat => Category.findByPk(req.params.id))
+  .then((updatedCategory)=> res.status(200).json(updatedCategory))
+  .catch((err) =>{res.json(err);});
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+ await Category.destroy({
+  where: {
+    id: req.params.id,
+  },
+ })
+ .then((rmvdCategory)=>{
+  res.json(`Category removed from the database`);
+ })
+ .catch((err)=>{
+  res.json(err);
+ });
 });
 
 module.exports = router;
