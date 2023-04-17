12a592d3de06c0beccfe34284611764600dbcdb7
Xhway <dhway20@gmail.com>
2023-04-17 18:49:40 -0400

    added delete route to product-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -67,7 +67,7 @@ router.post('/', (req, res) => {
   */
   Product.create(req.body)
     .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
+      
       if (req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
@@ -129,8 +129,19 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  await Product.destroy({
+    where:{
+      id: req.params.id,
+    },
+  })
+  .then((rmvdCategory)=>{
+    req.json(`Product removed from the database`);
+  })
+  .catch((err)=>{
+    res.json(err);
+  });
 });
 
 module.exports = router;
